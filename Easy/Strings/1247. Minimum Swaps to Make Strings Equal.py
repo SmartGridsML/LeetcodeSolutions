b543c649abd3bcdaa1895373684feec5cb40ba05
SmartGridsML <bilal.saleem_96@outlook.com>
2021-03-17 10:59:25 +0000

    initial commit

--- Easy/Strings/1247. Minimum Swaps to Make Strings Equal.py
+++ Easy/Strings/1247. Minimum Swaps to Make Strings Equal.py
@@ -4,7 +4,7 @@ Your task is to make these two strings equal to each other. You can swap any two
 that belong to different strings, which means: swap s1[i] and s2[j].
 Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if impossible.
 """
-def minimumSwap(self, s1: str, s2: str) -> int:
+def minimumSwap(s1: str, s2: str) -> int:
     
     xy_count = 0
     yx_count = 0
